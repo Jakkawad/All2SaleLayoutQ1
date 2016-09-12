2f9918f4718d2c299a0b862635b6f95f53e5aafe
Jakkawad <admin@all2sale-imac.local>
2016-09-12 12:42:27 +0700

    Initial Commit

---
+++ All2SaleLayoutQ1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  All2SaleLayoutQ1
+//
+//  Created by admin on 9/12/2559 BE.
+//  Copyright © 2559 All2Sale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
